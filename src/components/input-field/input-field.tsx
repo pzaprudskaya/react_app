8b001067c2c75eeef97ae7d39e8ea63746c6c4ae
PolinaZaprudskaya <43032222+PolinaZaprudskaya@users.noreply.github.com>
2020-02-14 10:46:24 +0300

    add debounce to input-field

--- src/components/input-field/input-field.tsx
+++ src/components/input-field/input-field.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import './input-field.scss';
-import {connect} from "react-redux";
 
 class InputField extends React.PureComponent<any, any> {
   constructor(props: any) {
@@ -8,18 +7,21 @@ class InputField extends React.PureComponent<any, any> {
     this.state = {
       placeholder: this.props.placeholder,
       type: this.props.type,
-      name: this.props.name
+      name: this.props.name,
+      value: this.props.value,
     };
   }
   private handleChange = (e: any): void => {
     const value = e.target.value;
-    const name = e.target.name;
+    this.setState({
+      value: value,
+    });
     this.props.onChange(this.props.name, value);
   };
 
   public render() {
     return (
-        <input type={this.state.type} placeholder={this.state.placeholder} name={this.state.name} className={'input'} value={this.props.value}
+        <input type={this.state.type} placeholder={this.state.placeholder} name={this.state.name} className={'input'} value={this.state.value}
                onChange={this.handleChange}/>
     )
   }
--- src/views/sign-up/steps/step-one/step-one.tsx
+++ src/views/sign-up/steps/step-one/step-one.tsx
@@ -5,6 +5,7 @@ import CardFooter from "../../../../components/card-footer/card-footer";
 import InputField from "../../../../components/input-field/input-field";
 import { updateUser } from "../../../../reducers/actions/user-actions";
 import { connect } from 'react-redux';
+import { debounce } from 'lodash';
 
 class StepOne extends React.Component<any, any> {
     constructor(props: any) {
@@ -14,22 +15,21 @@ class StepOne extends React.Component<any, any> {
         };
     }
 
-    handleChange = (field: string, value: string) => {
-        console.log(this.props);
+    handleChange = debounce((field: string, value: string) => {
         const { user } = this.props;
         const localUser = { ...user };
         localUser[field] = value;
         this.props.onUpdateUser(localUser);
-    }
+    }, 1000);
 
   public render() {
       return (
             <div className={'userInfo'}>
                 <CardHeader title={'Registration'} subtitle={'User information'}/>
                     <InputField type={'text'} onChange={this.handleChange} placeholder={'Name'} name={'name'} value={this.props.user.name}/>
-                    <InputField type={'text'} placeholder={'Surname'} name={'surname'}/>
-                    <InputField type={'email'} placeholder={'E-mail'} name={'email'}/>
-                    <InputField type={'password'} placeholder={'Password'} name={'password'}/>
+                    <InputField type={'text'} onChange={this.handleChange} placeholder={'Surname'} name={'surname'} value={this.props.user.surname}/>
+                    <InputField type={'email'} onChange={this.handleChange} placeholder={'E-mail'} name={'email'} value={this.props.user.email}/>
+                    <InputField type={'password'} onChange={this.handleChange} placeholder={'Password'} name={'password'} />
                 <CardFooter next={'/signup/step-two'}/>
             </div>
 );
